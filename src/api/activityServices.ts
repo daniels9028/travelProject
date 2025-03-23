a696acecc1c2342e8b15e613824cdec6732dd905
daniels9028 <daniels9028@student.untan.ac.id>
2025-03-23 13:14:48 +0700

    feat: Fixing rename credentials to credential file in activity type folder

--- src/api/activityServices.ts
+++ src/api/activityServices.ts
@@ -1,5 +1,5 @@
 import axios from "../axios/axios"
-import { ActivityByCategoryIdCredentials, ActivityByIdCredentials, CreateActivityCredentials, UpdateActivityCredentials } from "../types/activity/credentials"
+import { ActivityByCategoryIdCredentials, ActivityByIdCredentials, CreateActivityCredentials, UpdateActivityCredentials } from "../types/activity/credential"
 import { ActivityByCategoryIdResponse, ActivityByIdResponse, AllActivityResponse, CreateActivityResponse, DeleteActivityResponse, UpdateActivityResponse } from "../types/activity/response"
 import { DeleteCategoryCredentials } from "../types/category/credential"
 
