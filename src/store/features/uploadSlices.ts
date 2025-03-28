05c8e9cdbc8b865fdd916305819433de1c1ce962
daniels9028 <daniels9028@student.untan.ac.id>
2025-03-28 13:52:40 +0700

    feat: Add upload slices for redux. Testing with upload api services consist of upload image

---
+++ src/store/features/uploadSlices.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { uploadImageThunk } from "../thunks/uploadThunks";
+
+interface UploadState {
+  loading: boolean;
+  message: string | null;
+  url: string | null;
+}
+
+const initialState: UploadState = {
+  loading: false,
+  message: null,
+  url: null,
+};
+
+const uploadSlice = createSlice({
+  name: "upload",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(uploadImageThunk.pending, (state) => {
+        state.loading = true;
+        state.message = null;
+      })
+      .addCase(uploadImageThunk.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.message = (payload.message as string) ?? null;
+        state.url = (payload.url as string) ?? null;
+      })
+      .addCase(uploadImageThunk.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.message = (payload as string) ?? null;
+      });
+  },
+});
+
+export default uploadSlice.reducer;
--- src/store/thunks/uploadThunks.ts
+++ src/store/thunks/uploadThunks.ts
@@ -2,11 +2,21 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 import uploadServices from "../../api/uploadServices";
 import { UploadImageCredentials } from "../../types/upload/credential";
+import { UploadImageResponse } from "../../types/upload/response";
+import { AxiosError } from "axios";
 
-export const uploadImageThunk = createAsyncThunk("upload/upload-image", async (credentials: UploadImageCredentials, { rejectWithValue }) => {
-    try {
-        return await uploadServices.uploadImageService(credentials)
-    } catch (error) {
-        return rejectWithValue(error || "Error when upload image")
-    }
-})
+export const uploadImageThunk = createAsyncThunk<
+  UploadImageResponse,
+  UploadImageCredentials,
+  { rejectValue: string }
+>("upload/upload-image", async (credentials, { rejectWithValue }) => {
+  try {
+    return await uploadServices.uploadImageService(credentials);
+  } catch (error) {
+    const axiosError = error as AxiosError<{ message?: string }>;
+
+    return rejectWithValue(
+      axiosError.response?.data.message || "Error when upload image"
+    );
+  }
+});
